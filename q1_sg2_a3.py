3039c507be32771cdb65fb77caf0df0f6180a51b
Ducusin-Kryzchan-Gabriel <keducusin2030@clc.pshs.edu.ph>
2026-08-16 23:33:01 +0800

    Fix comparison operators for Chinese Zodiac calculation
    Corrected comparison operators from '=' to '==' for zodiac sign determination.

--- q1_sg2_a3.py
+++ q1_sg2_a3.py
@@ -1,29 +1,29 @@
 birth_year = int(input("What is your birth year?: "))
 
 if birth_year >= 1900:
-  if birth_year%12 = 0:
-   print("Your Chinese Zodiac Sign is: Pig (猪 / Zhū)")
-  elif birth_year%12 = 1:
-   print("Your Chinese Zodiac Sign is: Rat (鼠 / Shǔ)")
-  elif birth_year%12 = 2:
-   print("Your Chinese Zodiac Sign is: Ox (牛 / Niú)")
-  elif birth_year%12 = 3:
-   print("Your Chinese Zodiac Sign is: Tiger (虎 / Hǔ)")
-  elif birth_year%12 = 4:
-   print("Your Chinese Zodiac Sign is: Rabbit (兔 / Tù)")
-  elif birth_year%12 = 5:
-   print("Your Chinese Zodiac Sign is: Dragon (龙 / Lóng)")
-  elif birth_year%12 = 6:
-   print("Your Chinese Zodiac Sign is: Snake (蛇 / Shé)")
-  elif birth_year%12 = 7:
-   print("Your Chinese Zodiac Sign is: Horse (马 / Mǎ)")
-  elif birth_year%12 = 8:
-   print("Your Chinese Zodiac Sign is: Goat (羊 / Yáng)")
-  elif birth_year%12 = 9:
+  if birth_year%12 == 0:
    print("Your Chinese Zodiac Sign is: Monkey (猴 / Hóu)")
-  elif birth_year%12 = 10:
+  elif birth_year%12 == 1:
    print("Your Chinese Zodiac Sign is: Rooster (鸡 / Jī)")
-  elif birth_year%12 = 11:
+  elif birth_year%12 == 2:
+   print("Your Chinese Zodiac Sign is: Ox (牛 / Niú)")
+  elif birth_year%12 == 3:
    print("Your Chinese Zodiac Sign is: Dog (狗 / Gǒu)")
+  elif birth_year%12 == 4:
+   print("Your Chinese Zodiac Sign is: Rat (鼠 / Shǔ)")
+  elif birth_year%12 == 5:
+   print("Your Chinese Zodiac Sign is: Ox (牛 / Niú)")
+  elif birth_year%12 == 6:
+   print("Your Chinese Zodiac Sign is: Tiger (虎 / Hǔ)")
+  elif birth_year%12 == 7:
+   print("Your Chinese Zodiac Sign is: Rabbit (兔 / Tù)")
+  elif birth_year%12 == 8:
+   print("Your Chinese Zodiac Sign is: Dragon (龙 / Lóng)")
+  elif birth_year%12 == 9:
+   print("Your Chinese Zodiac Sign is: Snake (蛇 / Shé)")
+  elif birth_year%12 == 10:
+   print("Your Chinese Zodiac Sign is: Horse (马 / Mǎ)")
+  elif birth_year%12 == 11:
+   print("Your Chinese Zodiac Sign is: Goat (羊 / Yáng)")
 else:
   print("Invalid Year, it should not be earlier than 1900.")
