ac403cd5ff235d660f5a4e030dea22ff36ca540e
Ducusin-Kryzchan-Gabriel <keducusin2030@clc.pshs.edu.ph>
2026-09-18 08:23:35 +0800

    Fix HP variable name and update print statements
    Fixed variable name from 'hp' to 'HP' in the Hero class constructor and updated print statements to reflect damage taken and remaining HP.

--- q1_sg5_a1_Pinatubo_Ducusin.py
+++ q1_sg5_a1_Pinatubo_Ducusin.py
@@ -2,7 +2,7 @@ class Hero:
 
     def __init__(self, name, HP):
       self.name = name
-      self.hp = hp
+      self.hp = HP
     def take_damage(self, amount):
       self.hp -= amount
 
@@ -11,5 +11,8 @@ morgana = Hero("Morgana", 100)
 
 arthur.take_damage(10)
 
-print(f"{arthur.name}'s HP : {arthur.hp}")
-print(f"{morgana.name}'s HP : {morgana.hp}")
+print(f"{arthur.name} took damage!")
+print("")
+
+print(f"{arthur.name}'s Remaining HP : {arthur.hp}")
+print(f"{morgana.name}'s Remaining HP : {morgana.hp}")
